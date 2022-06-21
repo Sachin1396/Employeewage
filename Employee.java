97dd0bc2fe5e441fc02db9422a7555dffc24f556
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-21 13:10:49 +0530

    Repost:checking employe present or absent

--- Employee.java
+++ Employee.java
@@ -1,8 +1,17 @@
 package CoreJava;
 
+import Utility.Util;
+
+import java.util.Random;
+
 public class Employee {
     public static void main(String[] args)
     {
         System.out.println("Welcome to employee wage computation program on master branch");
+        Random random = new Random();
+        int x = random.nextInt(1000);
+        Util ref = new Util();
+        ref.present(x);
+
     }
 }
