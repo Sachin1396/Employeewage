a6d273d27aa7e74e3e360d4f41ff8428cf1838f7
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-21 12:46:58 +0530

    Add:Welcome message

---
+++ Employee.java
@@ -0,0 +1,8 @@
+package CoreJava;
+
+public class Employee {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to employee wage computation program on master branch");
+    }
+}
